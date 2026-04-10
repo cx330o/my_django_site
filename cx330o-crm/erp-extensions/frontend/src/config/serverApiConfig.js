b77a103ca3fb3f09abb0a3f1230df2aee701050a
2391881857 <2391881857@qq.com>
2026-04-10 18:15:10 +0900

    feat: PDF生成フロントエンドのレイアウト

---
+++ cx330o-crm/erp-extensions/frontend/src/config/serverApiConfig.js
@@ -0,0 +1,23 @@
+export const API_BASE_URL =
+  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
+    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
+    : 'http://localhost:8888/api/';
+export const BASE_URL =
+  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
+    ? import.meta.env.VITE_BACKEND_SERVER
+    : 'http://localhost:8888/';
+
+export const WEBSITE_URL = import.meta.env.PROD
+  ? 'https://cx330o.com/'
+  : 'http://localhost:3000/';
+export const DOWNLOAD_BASE_URL =
+  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
+    ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
+    : 'http://localhost:8888/download/';
+export const ACCESS_TOKEN_NAME = 'x-auth-token';
+
+export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
+
+//  console.log(
+//    '🚀 Welcome to cx330o DocForge! Did you know that we also offer commercial customization services? Contact us at admin@cx330o.com for more information.'
+//  );
